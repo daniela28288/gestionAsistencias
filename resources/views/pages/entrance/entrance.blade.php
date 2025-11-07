3bf68347a3f4730081ba24e355b10056f89a8e18
Kyoto <fjosedavid17@gmail.com>
2025-11-07 13:44:51 -0500

    se cambiaron los estilos del apartaddo de usuario de la vista de entrada

--- resources/views/pages/entrance/entrance.blade.php
+++ resources/views/pages/entrance/entrance.blade.php
@@ -36,6 +36,8 @@
                     <div id="full_hour"></div>
                     <div class="date-display" id="full_date"></div>
                 </div>
+
+                <div id="error_message"></div>
             </div>
 
             <div class="columna">
